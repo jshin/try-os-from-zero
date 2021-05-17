e0b8bce01a73b7086c1b9a345f97285c07136ed0
jshin <9659726+jshin@users.noreply.github.com>
2021-05-18 01:43:11 +0900

    added source codes

---
+++ MikanLoaderPkg/Main.c
@@ -0,0 +1,9 @@
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_hundle,
+                           EFI_SYSTEM_TABLE *system_table) {
+  Print(L"Yatteiki!\n");
+  while (1);
+  return EFI_SUCCESS;
+}
