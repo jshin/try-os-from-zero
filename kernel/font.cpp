c2081bd0c819c535ab4cf6f41eeeb20652760175
jshin <9659726+jshin@users.noreply.github.com>
2021-08-23 23:37:01 +0900

    added codes for day05d implemented WriteString function

--- kernel/font.cpp
+++ kernel/font.cpp
@@ -12,8 +12,9 @@ const uint8_t* GetFont(char c) {
     return &_binary_hankaku_bin_start + index;
 }
 
-void WriteAscii(PixelWriter &writer, int x, int y, char c, const PixelColor &color) {
-  const uint8_t* font = GetFont(c);
+void WriteAscii(PixelWriter &writer, int x, int y, char c,
+                const PixelColor &color) {
+  const uint8_t *font = GetFont(c);
   if (font == nullptr) {
     return;
   }
@@ -23,5 +24,12 @@ void WriteAscii(PixelWriter &writer, int x, int y, char c, const PixelColor &col
         writer.Write(x + dx, y + dy, color);
       }
     }
+  }
+}
+
+void WriteString(PixelWriter &writer, int x, int y, const char *s,
+                 const PixelColor &color) {
+  for (int i = 0; s[i] != '\0'; ++i) {
+    WriteAscii(writer, x + 8 * i, y, s[i], color);
   }
 }
