29a33f054fa3c45bec686502c9cf51d389f1c2e2
jshin <9659726+jshin@users.noreply.github.com>
2021-08-24 20:21:20 +0900

    added codes for day05f implemented printk function

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdarg>
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
@@ -13,6 +14,22 @@ void operator delete(void *obj) noexcept {}
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
+
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char *format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -32,28 +49,10 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     }
   }
 
-  // for (int x = 0; x < 200; ++x) {
-  //   for (int y = 0; y < 100; y++) {
-  //     pixel_writer->Write(x, y, {0, 255, 0});
-  //   }
-  // }
-  //
-  // int i = 0;
-  // for (char c = '!'; c < '~'; ++c, ++i) {
-	 //  WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
-  // }
-  // WriteString(*pixel_writer, 0, 66, "Hello world!", {0, 0, 255});
-  //
-  // char buf[128];
-  // sprintf(buf, "1 + 2 = %d", 1 + 2);
-  // WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
+  console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
-
-  char buf[128];
   for (int i = 0; i < 27; ++i) {
-      sprintf(buf, "line %d\n", i);
-      console.PutString(buf);
+      printk("printk: %d\n", i);
   }
 
   while (1)
